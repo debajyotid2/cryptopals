51b33dfa2ab0b9c672549667638518983c934a62
Debajyoti Debnath <ddebnath@carboncopies.org>
2024-04-24 19:44:49 -0400

    Implement hex to hex xor.

--- set1/src/lib.rs
+++ set1/src/lib.rs
@@ -89,6 +89,16 @@ pub fn base64tohex(base64: &String) -> String {
     bintohex(&base64tobin(base64))
 }
 
+pub fn hex_xor(buf1: &String, buf2: &String) -> String {
+    if buf1.len() != buf2.len() {
+        panic!("Buffers should be of the same length.");
+    }
+    let res: Vec<u8> = std::iter::zip(hextobin(buf1).chars(), hextobin(buf2).chars())
+                            .map(|(a, b)| (a as u8 - 48) ^ (b as u8 - 48) + 48)
+                            .collect();
+    bintohex(&String::from_utf8(res).unwrap())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -129,5 +139,13 @@ mod tests {
         let hex = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
         let base64 = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
         assert_eq!(base64tohex(&base64), hex);
+    }
+
+    #[test]
+    fn test_hex_xor() {
+        let hex1 = String::from("1c0111001f010100061a024b53535009181c");
+        let hex2 = String::from("686974207468652062756c6c277320657965");
+        assert_eq!(hex_xor(&hex1, &hex2), 
+            String::from("746865206b696420646f6e277420706c6179"));
     }
 }
