5c9f585d092d3fefeba0e66c43b4509552de9669
Debajyoti Debnath <ddebnath@carboncopies.org>
2024-04-29 12:54:57 -0400

    Add test with macbeth.txt

--- set1/src/main.rs
+++ set1/src/main.rs
@@ -1,9 +1,16 @@
+use std::fs;
 use set1::*;
 
 fn main() {
-    let ciphertext = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
-    let matches = decrypt_singlebyte_xor(&ciphertext);
-    for count in 0..5 {
-        println!("Match {}: {}", count + 1, &matches[count]);
-    }
+    // let ciphertext = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    let plaintext = fs::read_to_string("macbeth.txt").unwrap();
+    let bin_plaintext = asciitobin(&plaintext);
+    let key = 216u8;
+    let ciphertext = hex_xor(&bintohex(&bin_plaintext), 
+                            &bintohex(&format!("{:08b}", key)
+                                .repeat(bin_plaintext.len() / 8)));
+    let _matches = decrypt_singlebyte_xor(&ciphertext);
+    // for count in 0..5 {
+    //     println!("Match {}: {}", count + 1, &matches[count][0..200]);
+    // }
 }
