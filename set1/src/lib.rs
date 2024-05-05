0540647546d7d264f2e7191a854aacc3550cdb34
Debajyoti Debnath <ddebnath@carboncopies.org>
2024-05-05 10:56:43 -0400

    Add unit test for decrypt_repeatingkey_xor.

--- set1/src/lib.rs
+++ set1/src/lib.rs
@@ -611,4 +611,11 @@ mod tests {
         assert_eq!(encrypt_repeatingkey_xor(&plaintext, &key), 
                     String::from("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"));
     }
+
+    #[test]
+    fn test_decrypt_repeatingkey_xor() {
+        let ciphertext: Vec<u8> = hextobytearray(&String::from("514542204e52465a4820594f4c544b20434c552047524a4d50204c53424f20514542204958575620414c440a"));
+        let matches = decrypt_repeatingkey_xor(&ciphertext); 
+        assert_eq!(matches.len(), 5);
+    }
 }
